fe073e1d201637c37daa57e8244aedc4edfbe8b1
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-07-05 02:29:46 -0300

    refactor: Improved Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.*;
 import java.util.concurrent.*;
@@ -14,18 +15,24 @@ public class Main {
 		ExecutorService executor = Executors.newFixedThreadPool(1);
 
 		SwingUtilities.invokeLater(() -> {
+			Singleton s = Singleton.getInstance();
+
 			// Get screen size
 			Dimension screenSize = Singleton.getScreenSize();
 
-			Player player_1 = new Player("Player 1");
-			Player player_2 = new Player("Player 2");
+			Player player_1 = new Player("Player 1", Color.CYAN);
+			Player player_2 = new Player("Player 2", new Color(179, 33, 202));
 			Ball ball = new Ball(screenSize);
+
+			s.setPlayer1(player_1);
+			s.setPlayer2(player_2);
 
 			// Set players positions
 			player_1.setLocation(5, 5);
 			player_2.setLocation(screenSize.width - player_2.getPlayerDimension().width - 15, 5);
 			// Set ball position
-			ball.setLocation(screenSize.width / 2 - ball.getBallDimension().width / 2, screenSize.height / 2 - ball.getBallDimension().height / 2);
+			ball.setLocation(screenSize.width / 2 - ball.getBallDimension().width / 2,
+					screenSize.height / 2 - ball.getBallDimension().height / 2);
 
 			executor.submit(ball);
 
