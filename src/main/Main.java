66e363b1441c2d980ec58a16be21d9c5894221d9
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-07-06 00:31:49 -0300

    refactor: player dimensions on Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,26 +17,29 @@ public class Main {
 
 			// Get screen size
 			Dimension screenSize = Singleton.getScreenSize();
+			// Set players dimensions
+			Dimension playerDimension = new Dimension(125, 300);
 
-			Player player_1 = new Player("Player 1", Color.CYAN);
-			Player player_2 = new Player("Player 2", new Color(179, 33, 202));
+			Player player1 = new Player("Player 1", playerDimension, Color.CYAN);
+			Player player2 = new Player("Player 2", playerDimension, new Color(179, 33, 202));
 			Ball ball = new Ball();
 
-			s.setPlayer1(player_1);
-			s.setPlayer2(player_2);
+			s.setPlayer1(player1);
+			s.setPlayer2(player2);
 
 			// Set players positions
-			player_1.setLocation(5, 5);
-			player_2.setLocation(screenSize.width - player_2.getPlayerDimension().width - 15, 5);
+			player1.setLocation(5, 5);
+			player2.setLocation(screenSize.width - player2.getPlayerDimension().width - 15, 5);
 			// Set ball position
 			ball.setLocation(screenSize.width / 2 - ball.getBallDimension().width / 2,
 					screenSize.height / 2 - ball.getBallDimension().height / 2);
 
+			// Send ball to executor
 			executor.submit(ball);
 
 			// Set players visible
-			player_1.setVisible(true);
-			player_2.setVisible(true);
+			player1.setVisible(true);
+			player2.setVisible(true);
 			// Set ball visible
 			ball.setVisible(true);
 
