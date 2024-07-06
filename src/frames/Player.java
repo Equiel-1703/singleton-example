e605700acbad2ee32480e1aa69a170b16195dbcd
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-07-06 00:31:29 -0300

    fix: made score label visible

--- src/frames/Player.java
+++ src/frames/Player.java
@@ -7,28 +7,37 @@ import java.awt.Dimension;
 import java.awt.Color;
 
 public class Player extends JFrame {
-	private final Dimension player_dimension = new Dimension(125, 300);
 	private final int speed = 10;
+	private Dimension playerDimension;
 	private int score = 0;
+	private JLayeredPane layeredPane;
 
-	public Player(String title, Color c) {
+	public Player(String title, Dimension d, Color c) {
+		this.playerDimension = d;
+		this.layeredPane = new JLayeredPane();
+
 		setTitle(title);
-
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 
-		Rectangle rect = new Rectangle(player_dimension, c);
+		Rectangle rect = new Rectangle(playerDimension, c);
 		JLabel score_label = new JLabel("Score: " + score);
 
-		getContentPane().add(rect);
-		getContentPane().setForeground(Color.BLACK);
-		getContentPane().add(score_label);
+		score_label.setForeground(Color.BLACK);
+		score_label.setLocation(0, 0);
+		score_label.setSize(100, 50);
 
-		pack();
+
+		layeredPane.add(rect, JLayeredPane.DEFAULT_LAYER);
+		layeredPane.add(score_label, JLayeredPane.PALETTE_LAYER);
+
+		add(layeredPane);
+
+		setSize(rect.getSize());
 	}
 
 	public Dimension getPlayerDimension() {
-		return player_dimension;
+		return playerDimension;
 	}
 
 	public void moveUp() {
@@ -47,7 +56,7 @@ public class Player extends JFrame {
 		score++;
 
 		SwingUtilities.invokeLater(() -> {
-			JLabel score_label = (JLabel) getContentPane().getComponent(1);
+			JLabel score_label = (JLabel) layeredPane.getComponentsInLayer(JLayeredPane.PALETTE_LAYER)[0];
 			score_label.setForeground(Color.BLACK);
 			score_label.setBackground(Color.RED);
 			score_label.setText("Score: " + score);
